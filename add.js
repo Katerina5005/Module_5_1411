36093597b6b82630706393e02b04c6d2b0d6a83e
Katerina5005 <kat51082@gmail.com>
2022-11-14 21:27:30 +0600

    transition

--- add.js
+++ add.js
@@ -1,43 +1,30 @@
 
-
-
-
 function showPic_5() {
-    let pic_5 = document.getElementById('item_5')
-    pic_5.style.opacity = 1;
-    pic_5.style.position = 'absolute';
-    pic_5.style.transform = 'translate(530%)';
-    pic_5.style.transition = 'all 5s ease-in-out'; 
+    const pic_5 = document.getElementById('item_5');
+    pic_5.style.transition = 'all 3.5s ease-in-out'; 
+    pic_5.style.left = '78%';
   }
 
 function pic_4() {
     const pic_4 = document.getElementById('item_4')
-    pic_4.style.display =''; 
-    pic_4.style.position = 'absolute';
-    pic_4.style.transform = 'translate(400%)';
     pic_4.style.transition = 'all 3s ease-in-out';
+    pic_4.style.left = '60%';
 }
 
 function pic_3() {
     const pic_3 = document.getElementById('item_3')
-    pic_3.style.display =''; 
-    pic_3.style.position = 'absolute';
-    pic_3.style.transform = 'translate(270%)';
     pic_3.style.transition = 'all 2s ease-in-out';
+    pic_3.style.left = '42%';
 }
 
 function pic_2() {
     const pic_2 = document.getElementById('item_2')
-    pic_2.style.display =''; 
-    pic_2.style.position = 'absolute';
-    pic_2.style.transform = 'translate(140%)';
-    pic_2.style.transition = 'all 1s ease-in-out';
+    pic_2.style.transition = 'all 1.5s ease-in-out';
+    pic_2.style.left = '24%';
 }
 
 function pic_1() {
     const pic_1 = document.getElementById('item_1')
-    pic_1.style.display =''; 
-    //pic_1.style.position = 'absolute';
-    pic_1.style.transform = 'translate(10%)';
-    pic_1.style.transition = 'all .5s ease-in-out';
+    pic_1.style.transition = 'all 1s ease-in-out';
+    pic_1.style.left = '6%';
 }
